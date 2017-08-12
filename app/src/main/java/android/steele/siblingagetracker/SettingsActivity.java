893ef8887411460517fa91168b0ab764173ca4a1
Jonathan Steele <jonathan.steele232@gmail.com>
2017-08-11 18:43:01 -0600

    *WARNING* Last commit was an accidental half-commit, so I can't guarantee it's stable. This one has the setting activity and layout.

---
+++ app/src/main/java/android/steele/siblingagetracker/SettingsActivity.java
@@ -0,0 +1,38 @@
+package android.steele.siblingagetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private String TAG = this.getClass().toString();
+    private boolean notificationsOn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings2);
+
+        Switch s = (Switch) findViewById(R.id.switch1);
+        s.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Log.d(TAG, "Made it to the onclick event");
+
+        Log.d(TAG, "Click: Switch is: " + isChecked);
+
+    }
+//
+//    public void onNotificationsChange(View view) {
+//        Log.d(TAG, "Made it to the onclick event");
+//
+//        Log.d(TAG, "Click: Switch is: " + view.findViewById(R.id.switch1));
+//
+//    }
+}
