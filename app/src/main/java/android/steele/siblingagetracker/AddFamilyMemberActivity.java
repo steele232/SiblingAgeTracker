b58762be5be813d04bb241b6f45fcfa6f2678379
Jonathan Steele <jonathan.steele232@gmail.com>
2017-08-14 13:19:27 -0600

    THIS ONE has the AddFamilyMemberActivity and layout. Ready to start adding data.

---
+++ app/src/main/java/android/steele/siblingagetracker/AddFamilyMemberActivity.java
@@ -0,0 +1,13 @@
+package android.steele.siblingagetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddFamilyMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_family_member);
+    }
+}
