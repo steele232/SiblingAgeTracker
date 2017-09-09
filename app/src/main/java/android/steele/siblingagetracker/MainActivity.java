da882e3908be9a699ccbfbb96dcfd5ed2f3c2bab
Jonathan Steele <jonathan.steele232@gmail.com>
2017-09-08 21:48:57 -0600

    Got some sort of ListView going, seems to be just about the most effective way to do it with lifecycles. I might want to do a little launch screen so that the user doesn't have to see the wait before the listview loading. Some other animation may also help the user understand what's going on.wq

--- app/src/main/java/android/steele/siblingagetracker/MainActivity.java
+++ app/src/main/java/android/steele/siblingagetracker/MainActivity.java
@@ -2,6 +2,8 @@ package android.steele.siblingagetracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.steele.siblingagetracker.android.steele.siblingagetracker.model.FamilyMember;
+import android.steele.siblingagetracker.android.steele.siblingagetracker.model.FamilyMemberRow;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +12,24 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.toString();
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,48 +51,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //TEST THE FIREBASE DATABASE
-        // Write a message to the database
+        mListView = (ListView) findViewById(R.id.familyMembersList);
+    }
+
+    @Override
+    protected void onStart() {
+
+       super.onStart();
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference userRef = database.getReference("user2");
-        Log.d("TAG", userRef.getKey().toString());
-
-        // TODO: GET ALL THE OBJECTS AND SET UP THE LISTVIEW
-        // (Don't worry about getting the things more than once.. ?
-
-
-        //IDEA:
-        // have the listview in the XML already
-        // get the values in the OnCreate->ref.addValueListener--->>
-        //      Set up the data and create the ListView from the data.
-
         DatabaseReference familyMembersRef = userRef.child("familyMembers");
-        familyMembersRef.addValueEventListener(new ValueEventListener() {
+
+        FirebaseListAdapter<FamilyMemberRow> mAdapter = new FirebaseListAdapter<FamilyMemberRow>(this, FamilyMemberRow.class, R.layout.row_family_member, familyMembersRef) {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.d(TAG, "ListView Loading");
-                Toast.makeText(MainActivity.this, "ListView Loading", Toast.LENGTH_LONG);
-                //make an arrayList or something
-                //      of familyMembers
+            protected void populateView(View view, FamilyMemberRow myFamilyMemberRow, int position) {
+                //Set the value for the views
+                ((TextView)view.findViewById(R.id.name)).setText(myFamilyMemberRow.name);
 
-                // iterate through the dataSnapshot
-                //      Make a FamilyMember out of each row and
-                //      add them to the dataSnapShot
-
-                //sort the list?? maybe an added feature for later
-                //      sort by birthday first, for sure
-
-                //add that ArrayList / adapter? to the ListView,
-                //      (populate the listView)
-                //**** ALL WITHIN THE VALUE EVENT LISTENER!!! ******
-
+                //...
             }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        };
+        mListView.setAdapter(mAdapter);
 
     }
 
@@ -111,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/android/steele/siblingagetracker/android/steele/siblingagetracker/model/FamilyMemberRow.java
@@ -0,0 +1,10 @@
+package android.steele.siblingagetracker.android.steele.siblingagetracker.model;
+
+/**
+ * Created by jonathansteele on 9/8/17.
+ */
+
+public class FamilyMemberRow {
+    public String name;
+    public String birthdate;
+}
